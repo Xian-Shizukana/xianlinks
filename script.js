e7f7eff12dcf3e0ba16683e3c22e150255e57927
Xian Shizukana <138363403+Xian-Shizukana@users.noreply.github.com>
2024-02-09 21:58:09 +0800

    1.0

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+/* To Change Fonts Every 250ms
+var i = 0;
+var fonts = ["just_another_handregular", "boldly_missyregular", "danielregular", "playtime_with_hot_toddiesRg"];
+
+function changeFont(){
+    document.getElementById("headertext").style.fontFamily = fonts[i];
+    i++;
+    if(i > fonts.length) { i = 0; }
+}
+
+window.setInterval(changeFont, 250); */
+
+function youtube(){
+    window.location.href="https://www.youtube.com/channel/UCHawde7ag9YP-ePvXtnZc_w";
+}
+
+function twitter(){
+    window.location.href="https://twitter.com/xianshizukana";
+}
+function steam(){
+    window.location.href="https://steamcommunity.com/profiles/76561199111554317/";
+}
+function github(){
+    window.location.href="https://github.com/Xian-Shizukana";
+}
+function reddit(){
+    window.location.href="https://www.reddit.com/user/KillerBoy_667";
+}
+function lospec(){
+    window.location.href="https://lospec.com/xianshizukana";
+}
